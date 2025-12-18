1542d4e08a655155b9d874003bb83e504be73082
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-12-18 09:40:20 +0600

    spread file updated

--- src/spread.ts
+++ src/spread.ts
@@ -1,4 +1,4 @@
-// array er jonno spread operator
+// spread for array
 
 let friends:string[] = ["Moshiur"];
 
@@ -7,4 +7,22 @@ let schoolFriends:string[] = ["Sohan" , "Bipul", "Shamim", "Roni"];
 let collegeFriends: string[] = ["Sajedul", "Ontor", "Sanjid"];
 
 friends.push(...schoolFriends);
-console.log(friends);
+// console.log(friends);
+
+
+// spread for object
+
+const user = {
+    name: "Mezba",
+    phone: "0333344433",
+    location: "Chittagong"
+}
+
+const otherInfos = {
+    education: "DU",
+    stack: "MERN"
+}
+
+
+const userInfo = {...user, ...otherInfos};
+console.log(userInfo);
