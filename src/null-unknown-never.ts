e42f8ab7d76b5db64e965e5bf400ce1b98960181
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-12-18 22:20:25 +0600

    null unknown any done

---
+++ src/null-unknown-never.ts
@@ -0,0 +1,36 @@
+// 1. nullable
+
+const getUser = (input: string | null) => {
+    if(input) {
+        console.log(`From database we got : ${input}`);
+    }
+    else {
+        console.log(`From database : All User`);
+    }
+}
+
+
+// getUser(null);
+
+
+// 2. unknown
+
+const discountCalculator = (input: unknown ) =>{
+    if(typeof input === 'number') {
+        const finalPrice = input * 0.1;
+        console.log(finalPrice);
+    }
+    else if(typeof input === 'string') {
+        const [a,b] = input.split(' ');
+        const finalPrice = Number(a) * 0.1;
+        console.log(finalPrice);
+        
+    }
+    else{
+        console.log('Wrong Input')
+    }
+}
+
+discountCalculator(100);
+discountCalculator('100 taka');
+discountCalculator(null);
