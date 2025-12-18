6188a631bc048eef429bb1502da57916f4922656
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-12-18 06:51:46 +0600

    array , object, tuple done

---
+++ src/non-primitive.ts
@@ -0,0 +1,22 @@
+// array, object
+
+let bazaList : string[] = ['eggs', 'milk', 'sugar']; 
+// bazaList.push(5);
+
+
+let mixedArr : (string | number)[] = ['eggs', 12, "milk", 1, "sugar", 2];
+mixedArr.push('apple');
+
+// console.log(mixedArr);
+
+
+// tuple
+let couple: [string, string] = ["Husband", "wife"];
+// fixed length er ekta tuple
+
+
+
+let nameAndRoll : [string, number] =["Mezba", 79];
+
+let journey : [string, string, number] = ["Dhaka", "Chattogram", 7]
+
---
+++ src/primitive.ts
@@ -0,0 +1,6 @@
+let userName = "mezba123";
+let isAdmin = true;
+
+let x = undefined;
+
+// konokichu diye define na kore dile "any" type hoy.
