b0a7da46470ddf251972ad11e620e2d3847e1892
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-12-18 10:28:37 +0600

    rest done

--- src/rest.ts
+++ src/rest.ts
@@ -1,1 +1,8 @@
 
+
+const sendInvitations = (...friends: string[] )=> {
+    friends.forEach((friend: string) => console.log(`Invitation Sent to ${friend}`))
+}
+
+
+sendInvitations("Pintu", "Chintu", "Bulbul");
